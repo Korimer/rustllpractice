1e187a9e3eda2a39e03219885a8f4723296638eb
Korimer <thekclov@gmail.com>
2025-04-04 14:55:03 -0600

    implemented my own shaky drop

--- src/goodstack.rs
+++ src/goodstack.rs
@@ -27,25 +27,40 @@ impl <T> Stack<T> {
     }
 
     pub fn pop(&mut self) -> Option<T> {
-        let me = mem::replace(&mut self.head,Link::Tail);
-        return match me {
+        match mem::replace(&mut self.head,Link::Tail) {
             Link::Tail => None,
             Link::Body(thing) => {
-                let node= *thing;
-                let itm = node.value;
-                self.head = node.next;
-                Some(itm)
+                self.head = thing.next;
+                Some(thing.value)
             }
-        };
+        }
+    }
+}
+
+impl <T> Drop for Stack<T> {
+    fn drop(&mut self) {
+        let mut current = &mut self.head;
+        while let Link::Body(thing) = current {
+            let mut next = mem::replace(&mut thing.next, Link::Tail);
+            mem::swap(&mut next, &mut current);
+        }
     }
 }
 
 pub fn main() {
     let mut a = Stack::new();
+    let mut b = Stack::new();
+    let mut c = Stack::new();
     for i in 0..10 {
         a.push(i);
+        b.push(i);
+        c.push(i);
     }
-    for i in 0..11 {
+    for _i in 0..11 {
         println!("{:?}",a.pop());
     }
+    while let Some(num) = b.pop() {
+        println!("{}",num);
+    }
+    drop(c);
 }
